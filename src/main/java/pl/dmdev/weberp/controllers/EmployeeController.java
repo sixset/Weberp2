2da2e5746d58e93819bfc4f21dfc9c8620851b8a
SixSet <d.martyniak@dm-dev.pl>
2018-02-08 21:16:36 +0100

    Init sql

--- src/main/java/pl/dmdev/weberp/controllers/EmployeeController.java
+++ src/main/java/pl/dmdev/weberp/controllers/EmployeeController.java
@@ -17,40 +17,40 @@ import java.util.List;
 @Controller
 public class EmployeeController {
 
-@Autowired
-EmployyeSerwice employyeSerwice;
-@Autowired
+    @Autowired
+    EmployyeSerwice employyeSerwice;
+    @Autowired
     EmployeeRepository employeeRepository;
 
     @RequestMapping("/employees")
-    public String getEmployee(Model model){
-       List <Employee> allEmploye = employyeSerwice.getAllEmploye();
-       model.addAttribute("employees",allEmploye);
+    public String getEmployee(Model model) {
+        List<Employee> allEmploye = employyeSerwice.getAllEmploye();
+        model.addAttribute("employees", allEmploye);
         return "employees";
     }
 
     @RequestMapping("/newemployee")
-    public String createEmployee(Model model){
-        model.addAttribute("employe",employyeSerwice.createEmptyEmployee());
+    public String createEmployee(Model model) {
+        model.addAttribute("employe", employyeSerwice.createEmptyEmployee());
         return "newemployee";
     }
 
     @RequestMapping("/editemployee")
-    public String editEmployee(@RequestParam("id") Integer id, Model model){
+    public String editEmployee(@RequestParam("id") Integer id, Model model) {
         Employee employee = employyeSerwice.getEmployee(id);
-        model.addAttribute("editemployee",employee);
+        model.addAttribute("editemployee", employee);
         return "editemployee";
     }
 
-    @RequestMapping(value = "/editemployee",method = RequestMethod.POST)
-    public String saveEditEmployee(Employee editemploye){
+    @RequestMapping(value = "/editemployee", method = RequestMethod.POST)
+    public String saveEditEmployee(Employee editemploye) {
         employyeSerwice.saveEditEmploye(editemploye);
         return "redirect:/employees";
     }
 
-    @RequestMapping(value = "/employees",method = RequestMethod.POST)
-    public String saveEmployee(Employee employee){
-        employeeRepository.saveEditEmploye(employee.getId(),employee);
+    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+    public String saveEmployee(Employee employee) {
+        employeeRepository.saveEditEmploye(employee.getId(), employee);
         return "redirect:/employees";
     }
 }
